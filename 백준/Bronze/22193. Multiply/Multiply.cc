c20aab13cd5340bc738cb4605ac22f735870e0bb
KIM SUN UK <syi05003@gmail.com>
2024-10-04 12:24:02 +0900

    [Bronze V] Title: Multiply, Time: 0 ms, Memory: 2020 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N, M;
+
+int main() {
+    cin >> N >> M;
+
+    long long A, B;
+
+    cin >> A >> B;
+
+    cout << A * B << "\n";
+    return 0;
+}
