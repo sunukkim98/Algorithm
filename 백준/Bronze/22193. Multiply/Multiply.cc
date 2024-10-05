cb5907bf805466e39cdb124492faf228cdc0236b
KIM SUN UK <syi05003@gmail.com>
2024-10-05 15:12:18 +0900

    [Bronze V] Title: Multiply, Time: 956 ms, Memory: 2388 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.cc
+++ 백준/Bronze/22193. Multiply/Multiply.cc
@@ -1,17 +1,43 @@
+
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-int N, M;
-
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
     cin >> N >> M;
-
-    long long A, B;
-
+    char A[50000], B[50000];
     cin >> A >> B;
 
-    cout << A * B << "\n";
+    int result[100000];
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            int temp = (A[N-1-j]-'0') * (B[M-1-i]-'0');
+            result[N+M-1-i-j] += temp;
+        }
+    }
+
+    for (int i = N + M - 1; i > 0; i--) {
+        result[i-1] += result[i] / 10;
+        result[i] %= 10;
+    }
+
+    bool flag = false;
+    for (int i = 0; i < N + M; i++) {
+        if (!flag) {
+            if (result[i] != 0)
+                flag = true;
+            else if (i == N + M - 1)
+                cout << "0";
+        }
+        if (flag)
+            cout << result[i];
+    }
+
     return 0;
 }
+
