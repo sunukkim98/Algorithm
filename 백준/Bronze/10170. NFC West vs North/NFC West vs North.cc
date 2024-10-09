c9601fa7682473df8ef7a21deb30acbd2811e8fb
KIM SUN UK <syi05003@gmail.com>
2024-10-09 12:13:33 +0900

    [Bronze V] Title: NFC West vs North, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string output = "NFC West       W   L  T\n-----------------------\nSeattle        13  3  0\nSan Francisco  12  4  0\nArizona        10  6  0\nSt. Louis      7   9  0\n\nNFC North      W   L  T\n-----------------------\nGreen Bay      8   7  1\nChicago        8   8  0\nDetroit        7   9  0\nMinnesota      5  10  1";
+using namespace std;
+
+
+int main() {
+    cout << output;
+
+    return 0;
+}
