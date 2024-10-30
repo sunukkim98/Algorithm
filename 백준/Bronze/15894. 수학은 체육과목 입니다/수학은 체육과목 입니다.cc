4dfc71ce8cc4d56a5b3620b1738b8f12d7852ff3
KIM SUN UK <syi05003@gmail.com>
2024-10-30 19:17:16 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long N;
+    cin >> N;
+
+    cout << N * 4;
+
+    return 0;
+}
