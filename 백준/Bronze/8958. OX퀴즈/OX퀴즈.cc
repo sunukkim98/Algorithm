6b037a1b830068788eafc94dc8f0ba49ba884892
Sunuk Kim <syi05003@gmail.com>
2024-12-04 14:32:16 +0900

    [Bronze II] Title: OX퀴즈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -0,0 +1,61 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int GCD(int x, int y) {
+    if (y == 0)
+        return x;
+    return GCD(y, x % y);
+}
+
+bool is_prime(long long n) {
+    if (n <= 1) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (long long i = 5; i * i <= n; i++) {
+        if (n % i == 0 || n % (i + 2) == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool compare(int x, int y) {
+    return y < x;
+}
+
+int main() {
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    string ox_result = "";
+    int O_cnt = 0;
+    int result = 0;
+    for (int i = 0; i < t; i++) {
+        cin >> ox_result;
+        for (int j = 0; j < ox_result.size(); j++) {
+            if (ox_result[j]  == 'O') {
+                O_cnt++;
+                // cout << O_cnt << " ";
+                result += O_cnt;
+            }else {
+                O_cnt = 0;
+            }
+        }
+        cout << result << "\n";
+        result = 0;
+        O_cnt = 0;
+    }
+
+    return 0;
+}
