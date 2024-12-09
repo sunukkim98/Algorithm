f6a4128909a5bdc2cca250a2d04d55811cad93c7
Sunuk Kim <syi05003@gmail.com>
2024-12-09 17:59:50 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include<stack>
+
+using namespace std;
+
+int GCD(int x, int y) {
+    if (y == 0)
+        return x;
+    return GCD(y, x % y);
+}
+
+bool check_prime(long long n) {
+    if (n <= 1) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (long long i = 5; i * i <= n; i++) {
+        if (n % i == 0 || n % (i + 2) == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool compare(int x, int y) {
+    return y < x;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int x, y;
+    cin >> x >> y;
+
+    cout << GCD(x,y) << "\n";
+    cout << x * y / GCD(x,y);
+
+    return 0;
+}
