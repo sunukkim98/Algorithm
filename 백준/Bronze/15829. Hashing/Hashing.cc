caa8198eec1a2d2442478d3df82e7c1f1381a795
Sunuk Kim <syi05003@gmail.com>
2024-12-22 20:45:13 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,48 +1,20 @@
 #include <iostream>
-#include <cmath>
+#include <string>
+#define r 31
+#define M 1234567891
 
 using namespace std;
 
-int GCD(int x, int y) {
-    if (y == 0)
-        return x;
-    return GCD(y, x % y);
-}
+int main () {
+    int l, i;
+    long long hash = 0, R = 1;
+    string str;
 
-bool check_prime(long long n) {
-    if (n <= 1) return false;
-    if (n == 2 || n == 3) return true;
-    if (n % 2 == 0 || n % 3 == 0) return false;
-    for (long long i = 5; i * i <= n; i++) {
-        if (n % i == 0 || n % (i + 2) == 0){
-            return false;
-        }
+    cin >> l >> str;
+
+    for (i = 0; i < str.length(); i++) {
+        hash += ((str[i] - 'a' + 1) * R) % M; hash %= M;
+        R = (R * r) % M;
     }
-    return true;
-}
-
-bool compare(int x, int y) {
-    return y < x;
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
-    int length;
-    cin >> length;
-
-    int hash_val = 0;
-    for (int i = 0; i < length; i++) {
-        char alpahbet;
-        cin >> alpahbet;
-        int alphabet_to_int = int(alpahbet) - 96;
-        // cout << alphabet_to_int;
-        hash_val += alphabet_to_int * pow(31,i);
-    }
-
-    cout << hash_val;
-
-    return 0;
+    cout << hash;
 }
