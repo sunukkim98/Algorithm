0559c15f1e9d1364525e769b391c97a96aa50150
Sunuk Kim <syi05003@gmail.com>
2025-01-01 13:48:24 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,46 @@
+/*
+  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \_________/
+    \_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \__
+ /             \
+/_______________\
+ */
+
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> arr;
+
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << "  ___  ___  ___\n";
+    cout << "  | |__| |__| |\n";
+    cout << "  |           |\n";
+    cout << "   \\_________/\n";
+    cout << "    \\_______/\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |_____|\n";
+    cout << "  __/       \\__\n";
+    cout << " /             \\\n";
+    cout << "/_______________\\\n";
+
+    return 0;
+}
