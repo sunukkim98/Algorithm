cea631df77b9abf089413b7c1812a22b0436c416
Sunuk Kim <sunukkim@soongsil.ac.kr>
2025-07-09 10:50:32 +0900

    [Bronze II] Title: 사각형 면적, Time: 36 ms, Memory: 32412 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/22341. 사각형 면적/사각형 면적.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n, c = map(int, input().split())
+width = n
+height = n
+area = width * height
+
+for _ in range(c):
+    x, y = map(int, input().split())
+
+    # 가로로 자를 경우
+    temp_height = height-(height-x)
+    temp_area = width * temp_height
+    # print(temp_area)
+
+    # 새로로 자를 경우
+    temp_width = width - (width - y)
+    temp_area_2 = temp_width * height
+    # print(temp_area_2)
+
+    if temp_area >= temp_area_2:
+        height = temp_height
+        area = temp_area
+    else:
+        width = temp_width
+        area = temp_area_2
+print(area)
