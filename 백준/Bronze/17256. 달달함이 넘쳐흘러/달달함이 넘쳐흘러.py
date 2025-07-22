1e9089e56f1312c79b741348189a251e00f7a959
Sunuk Kim <sunukkim@soongsil.ac.kr>
2025-07-22 23:37:52 +0900

    [Bronze IV] Title: 달달함이 넘쳐흘러, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+a_x, a_y, a_z = map(int, input().split())
+c_x, c_y, c_z = map(int, input().split())
+
+b_x = c_x - a_z
+b_y = c_y // a_y
+b_z = c_z - a_x
+
+print(b_x, b_y, b_z)
