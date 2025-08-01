75aad576de60cc845e20f39001cd5fa707e19b5f
Sunuk Kim <sunukkim@soongsil.ac.kr>
2025-08-01 18:01:47 +0900

    [Bronze IV] Title: HOMWRK, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/18398. HOMWRK/HOMWRK.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input().strip())
+for _ in range(t):
+    n = int(input().strip())
+    for _ in range(n):
+        a, b = map(int, input().split())
+        print(a+b, a*b)
