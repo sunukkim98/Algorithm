e1228a6df366e9f5dc1a298f15b16a461fcea136
Sunuk Kim <sunukkim@soongsil.ac.kr>
2025-08-07 20:20:27 +0900

    [Bronze IV] Title: 임스의 메이플컵, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/29790. 임스의 메이플컵/임스의 메이플컵.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, u, l = map(int, input().split())
+
+if n >= 1000 and (u >= 8000 or l >= 260):
+    print("Very Good")
+elif n >= 1000:
+    print("Good")
+else:
+    print("Bad")
