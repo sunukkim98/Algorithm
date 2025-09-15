9ddd1b9f0642cbd866c455d8ae41bf59aa2d2c99
Sunuk Kim <sunukkim@soongsil.ac.kr>
2025-09-15 11:46:54 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 60 ms, Memory: 34876 KB -BaekjoonHub

---
+++ 백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.py
@@ -0,0 +1,12 @@
+import sys
+# sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+# import math
+from collections import deque
+
+# 다음 풀 문제: 실버 4
+
+m = int(input().strip())
+k = int(input().strip())
+c = int(input().strip())
+print(m * c)
