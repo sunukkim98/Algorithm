9e758cd5d16ebea2e2e2ce000d116f537cffc93f
Sunuk Kim <sunukkim@soongsil.ac.kr>
2025-09-15 11:44:05 +0900

    [Bronze V] Title: Class Photo, Time: 56 ms, Memory: 34876 KB -BaekjoonHub

---
+++ 백준/Bronze/34281. Class Photo/Class Photo.py
@@ -0,0 +1,11 @@
+import sys
+# sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+# import math
+from collections import deque
+
+# 다음 풀 문제: 실버 4
+
+w = int(input().strip())
+l = int(input().strip())
+print(w * l)
